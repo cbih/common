0814ad00193dc8d804210e578a6a6a3982b1c584
Alexey Sidorov <cbih315@gmail.com>
2023-10-06 16:46:51 +0300

    Add files via upload

---
+++ src/cbih/common/Filesystem.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * cbih common library
+ *
+ * @package cbih\common
+ * @version $Id$
+ * @author Sidorov Aleksey <cbih315@gmail.com>
+ */
+
+namespace cbih\common;
+
+/**
+ * Набор функций для облегчения работы с файлами
+ */
+class Filesystem {
+
+  /**
+   * @param $dir string
+   * @param $mask string
+   * @param $result string
+   * @return Array
+   */
+  function getDirContents($dir, $mask = '', &$result = array()) {
+    $files = scandir($dir);
+    foreach ($files as $value) {
+      $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+      if (!is_dir($path)) {
+        if ($mask) {
+          if (preg_match($mask, $path)) {
+            $result[] = $path;
+          }
+        } else {
+          $result[] = $path;
+        }
+      } else if ($value != "." && $value != "..") {
+        getDirContents($path, $mask, $result);
+      }
+    }
+
+    return $result;
+  }
+
+}
