2b0207089e3df4a2380898cf08fc5fa4ae459df4
Alexey Sidorov <cbih315@gmail.com>
2023-11-28 17:16:29 +0300

    Update Filesystem.php - + pathByNum

--- src/cbih/common/Filesystem.php
+++ src/cbih/common/Filesystem.php
@@ -13,32 +13,48 @@ namespace cbih\common;
 /**
  * Набор функций для облегчения работы с файлами
  */
-class Filesystem {
+class Filesystem
+{
 
-  /**
-   * @param $dir string
-   * @param $mask string
-   * @param $result string
-   * @return Array
-   */
-  public static function getDirContents($dir, $mask = '', &$result = array()) {
-    $files = scandir($dir);
-    foreach ($files as $value) {
-      $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
-      if (!is_dir($path)) {
-        if ($mask) {
-          if (preg_match($mask, $path)) {
-            $result[] = $path;
-          }
-        } else {
-          $result[] = $path;
+    /**
+     * @param $dir string
+     * @param $mask string
+     * @param $result string
+     * @return Array
+     */
+    public static function getDirContents($dir, $mask = '', &$result = array())
+    {
+        $files = scandir($dir);
+        foreach ($files as $value) {
+            $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+            if (!is_dir($path)) {
+                if ($mask) {
+                    if (preg_match($mask, $path)) {
+                        $result[] = $path;
+                    }
+                } else {
+                    $result[] = $path;
+                }
+            } else if ($value != "." && $value != "..") {
+                getDirContents($path, $mask, $result);
+            }
         }
-      } else if ($value != "." && $value != "..") {
-        getDirContents($path, $mask, $result);
-      }
+
+        return $result;
     }
 
-    return $result;
-  }
+    /**
+     * calc 3-octet path
+     * @param $num integer
+     * @param $result string
+     * @return Array
+     */
+    public static function getPathByNum($num)
+    {
+        $num = str_pad($num, 12, '0', STR_PAD_LEFT);
+        $arr = str_split($num, 3);
+        $path = $arr[0] . '/' . $arr[1] . '/' . $arr[2] . '/' . $num;
+        return $path;
+    }
 
 }
